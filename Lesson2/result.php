f6b753936bc69944cd91ad5564388f82be53ce9e
Son dep trai <11691nhphuc@gmail.com>
2015-11-04 01:42:36 +0700

    commitL2

--- Lesson2/result.php
+++ Lesson2/result.php
@@ -4,26 +4,28 @@
 		<script type="text/javascript">alert("Please enter n : 0 < n <50");</script><?php
 		header("Location: http://localhost/first_project/Lesson2");
 	}elseif ($_POST['txt'] > 0 && $_POST['txt'] < 50) {
-		/*for ($i=0; $i <= $_POST['txt']; $i++) { 
-			if($i !=0){
-				echo " , " .$i;
-			}elseif ($i === 0) {
-				echo $i;
-			}
-		}*/
+
 	}
-	$str = array();
-	$str = $_POST['txt'];
-	$arr_ = explode(",", $str);
+
+	//$str = array();
+	$str = $_POST['txt']; // Mảng user nhập
+	$arr_ = explode(",", $str); // Mảng sau khi đã lọc dấu ,
+	$arr_length = count($arr_); // Tổng số pt mảng
+	
 	echo("Array you typed : " .$str);
 	echo "<pre>";
-	print_r($arr_));
+	print_r($arr_);
 	echo "</pre>";
 
 	$arr_max = print_r(max($arr_), true);
 	echo("Max element in array (use function max) : " .$arr_max. '<br>');
-
-	$arr_length = count($arr_);
+	$arr_max_2 = 0;
+	for ($i = 0; $i < $arr_length; $i++) { 
+		if($arr_[$i] > $arr_[$arr_max_2]){
+			$arr_max_2 = $i;
+		}
+	}
+	echo("Max element in array (use algorithm) : " .$arr_[$arr_max_2]. '<br>');
 	echo("Total elements of array : " .$arr_length. '<br>');
 
 	$count_max = 0;
