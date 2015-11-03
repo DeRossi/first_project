f416722d2f052c80f0d97f9ad922197790fe30e3
Son dep trai <11691nhphuc@gmail.com>
2015-11-04 01:52:43 +0700

    commit L2 httpredirect

--- Lesson2/result.php
+++ Lesson2/result.php
@@ -11,7 +11,7 @@
 	$str = $_POST['txt']; // Mảng user nhập
 	$arr_ = explode(",", $str); // Mảng sau khi đã lọc dấu ,
 	$arr_length = count($arr_); // Tổng số pt mảng
-	
+
 	echo("Array you typed : " .$str);
 	echo "<pre>";
 	print_r($arr_);
@@ -21,6 +21,9 @@
 	echo("Max element in array (use function max) : " .$arr_max. '<br>');
 	$arr_max_2 = 0;
 	for ($i = 0; $i < $arr_length; $i++) { 
+		if($arr_[$i] >= 50 || $arr_[$i] < 0){
+			header("Location: http://localhost/first_project/Lesson2");
+		}
 		if($arr_[$i] > $arr_[$arr_max_2]){
 			$arr_max_2 = $i;
 		}
