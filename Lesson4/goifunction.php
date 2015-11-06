22032e043dbb42509c44e314a2b82e698447c302
Happy <happy@cybridge.jp>
2015-11-06 19:59:42 +0900

    commit

--- Lesson4/goifunction.php
+++ Lesson4/goifunction.php
@@ -1,12 +1,7 @@
 <?php
 	function giatri_chan_trong_mang(array $arr_1){
-		/*foreach ($arr_1 as $key => $value) {
-			if($arr_1[$value] % 2 === 0){
-				$arr_chan1 = $arr_chan1[$arr_1[$value]];
-			}
-		}*/
 		for ($i=0; $i < count($arr_1) ; $i++) {
-			if($arr_1[$i] % 2 === 0){
+			if($arr_1[$i] % 2 == 0){
 				$arr_chan1[$i] = $arr_1[$i];
 			}
 		}
@@ -15,7 +10,8 @@
 
 	function giatri_max_trong_mang($arr_1){
 		$max_element = 0;
-		for ($i=0; $i < count($arr_1) ; $i++) {
+		$count = count($arr_1);
+		for ($i=0; $i < $count ; $i++) {
 			if($arr_1[$i] > $arr_1[$max_element]){
 				$max_element = $i;
 			}
