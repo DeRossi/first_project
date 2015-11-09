7f4c0833e0a8612e30e2f182d718b794ef8a4a65
Happy <happy@cybridge.jp>
2015-11-09 12:23:19 +0900

    add comment

--- Lesson4/goifunction.php
+++ Lesson4/goifunction.php
@@ -1,4 +1,5 @@
 <?php
+	// Bai 132
 	function giatri_chan_trong_mang(array $arr_1){
 		$count = count($arr_1);
 		for ($i=0; $i < $count ; $i++) {
@@ -8,7 +9,7 @@
 		}
 		return $arr_chan1;
 	}
-
+	// Bai 133
 	function cacvitri_co_GT_am_trong_mang(array $arr_1){
 		$flag_GT_am = false;
 		$count = count($arr_1);
@@ -24,7 +25,7 @@
 			return -1;
 		}
 	}
-
+	// Bai 134
 	function giatri_max_trong_mang(array $arr_1){
 		$max_element = 0;
 		$count = count($arr_1);
@@ -35,7 +36,7 @@
 		}
 		return $arr_1[$max_element];
 	}
-
+	// Bai 135
 	function giatri_duong_dautien_trong_mang(array $arr_1){
 		$flag_GTDuong_off = true;
 		$count = count($arr_1);
@@ -52,7 +53,7 @@
 			return $first_plus;
 		}
 	}
-
+	// Bai 136
 	function so_chan_cuoi_cung(array $arr_1){ //
 		$count = count($arr_1);
 		$cursor_max = 0;
@@ -66,7 +67,7 @@
 		}
 		return $max3;
 	}
-
+	// Bai 137
 	function vitri_co_giatri_nhonhat(array $arr_1){
 		$min = 0;
 		$count = count($arr_1);
@@ -77,7 +78,7 @@
 		}
 		return $min;
 	}
-
+	// Bai 138
 	function vitri_co_giatri_chan_dautien(array $arr_1){
 		$test_chan = 0;
 		$flag_chan_off = true;
