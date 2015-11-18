887e38d003efb10a9bb7e421a3e37a499077bcf1
vihoangson <vihoangson@gmail.com>
2015-11-18 19:23:20 +0700

    Step 3

--- sample_matrix/object_matrix.php
+++ sample_matrix/object_matrix.php
@@ -1,9 +1,12 @@
 <?php 
 require(__DIR__."/../library/class/matrix.php") ;
 $environment_1 = new Evironment;
-
+$environment_2 = new Evironment;
 $matrix_1 = new Matrix;
+
 $matrix_1->create_evironment($environment_1);
+
+$matrix_1->create_evironment($environment_2);
 
 $matrix_1->show();
 
@@ -12,5 +15,11 @@ $matrix_1->mye[0]->show();
 $matrix_1->mye[0]->change_color();
 
 $matrix_1->mye[0]->show();
+
+$matrix_1->mye[1]->show();
+
+$matrix_1->mye[1]->change_color();
+
+$matrix_1->mye[1]->show();
 
 ?>
